babec98ae4dd937101d2504ee02798db7b84eb2c
masonalan <jamespickering@tyson.lan>
2023-04-21 10:31:31 -0700

    Improve responsiveness of dark section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import {
 } from "@angular/core";
 
 const TITLE_ANIM_THRESHOLD = 100;
-const LOGO_MARGIN = 0;
 const FADE_DURATION = 200; /* in px */
 const TOOLBAR_HEIGHT = 125;
-
+const LOGO_MARGIN = 70;
+const PADDING = 30;
 const TWEET_ROTATIONS = [20, -20, 4, -20, 20];
 
 @Component({
@@ -21,9 +21,11 @@ const TWEET_ROTATIONS = [20, -20, 4, -20, 20];
 export class AppComponent implements AfterViewInit {
 	title = "enron-website";
 
-	yOffset = 0;
+	yCurr = 0;
+	yPrev = 0;
 
-	logo = {
+	logoInfo = {
+		top: 0,
 		bottom: 0,
 		fadeOutY: Number.MAX_SAFE_INTEGER,
 	};
@@ -38,8 +40,8 @@ export class AppComponent implements AfterViewInit {
 		fixedY: 0,
 	};
 
-	fadeInAt(threshold: number, duration = FADE_DURATION) {
-		const v = (this.yOffset - threshold) / duration;
+	posLinearSeq(threshold: number, duration = FADE_DURATION) {
+		const v = (this.yCurr - threshold) / duration;
 		if (v < 0) {
 			return 0;
 		} else if (v > 1) {
@@ -48,20 +50,128 @@ export class AppComponent implements AfterViewInit {
 		return v;
 	}
 
-	fadeOutAt(threshold: number, duration = FADE_DURATION) {
-		const v = (threshold - this.yOffset) / duration;
+	negLinearSeq(threshold: number, duration = FADE_DURATION) {
+		const v = (threshold - this.yCurr) / duration;
 		if (v < 0) {
 			return 0;
 		} else if (v > 1) {
 			return 1;
 		}
 		return v;
+	}
+
+	set(elem: ElementRef, fn: any) {
+		const clone = this._frozen.get(elem);
+		if (clone && clone.visible) {
+			fn(clone);
+		} else {
+			fn(elem.nativeElement);
+		}
+	}
+
+	_fadeIn = new Map();
+	fadeIn(elem: ElementRef, t: number, duration = FADE_DURATION) {
+		const isActive = this._fadeIn.get(elem);
+
+		if (this.yCurr >= t && this.yCurr <= t + duration) {
+			/**
+			 * yCurr is within anim range
+			 */
+			if (!isActive) {
+				this._fadeIn.set(elem, true);
+			}
+			this.set(elem, (e: any) => {
+				e.style.opacity = this.posLinearSeq(t, duration);
+			});
+		} else if (this.yCurr < t) {
+			/**
+			 * yCurr is above anim range
+			 */
+			if (isActive || this.yPrev > t + duration) {
+				this.set(elem, (e: any) => {
+					e.style.opacity = 0;
+				});
+				this._fadeIn.set(elem, false);
+			}
+		} else if (this.yCurr > t + duration && isActive) {
+			/**
+			 * yCurr is below anim range
+			 */
+			if (isActive || this.yPrev < t) {
+				this.set(elem, (e: any) => {
+					e.style.opacity = 1;
+				});
+				this._fadeIn.set(elem, false);
+			}
+		}
+	}
+
+	_fadeOut = new Map();
+	fadeOut(elem: ElementRef, t: number, duration = FADE_DURATION) {
+		const isActive = this._fadeOut.get(elem);
+
+		if (this.yCurr >= t - duration && this.yCurr <= t) {
+			/**
+			 * yCurr is within anim range
+			 */
+			if (!isActive) {
+				this._fadeOut.set(elem, true);
+			}
+			this.set(elem, (e: any) => {
+				e.style.opacity = this.negLinearSeq(t, duration);
+			});
+		} else if (this.yCurr < t - duration) {
+			/**
+			 * yCurr is above anim range
+			 */
+			if (isActive || this.yPrev > t) {
+				this.set(elem, (e: any) => {
+					e.style.opacity = 1;
+				});
+				this._fadeOut.set(elem, false);
+			}
+		} else if (this.yCurr > t && isActive) {
+			/**
+			 * yCurr is below anim range
+			 */
+			if (isActive || this.yPrev < t - duration) {
+				this.set(elem, (e: any) => {
+					e.style.opacity = 0;
+				});
+				this._fadeOut.set(elem, false);
+			}
+		}
+	}
+
+	_frozen = new Map();
+	freeze(elem: ElementRef, y: number) {
+		let clone = this._frozen.get(elem);
+		if (this.yCurr >= y) {
+			if (clone === undefined) {
+				clone = elem.nativeElement.cloneNode(true);
+				clone.style.position = "fixed";
+				clone.style.top = `${elem.nativeElement.offsetTop - y}px`;
+				clone.style.marginTop = `0px`;
+				elem.nativeElement.after(clone);
+				clone["visible"] = true;
+				this._frozen.set(elem, clone);
+				elem.nativeElement.style.opacity = 0;
+			} else if (!clone.visible) {
+				clone.style.display = "flex";
+				clone.visible = true;
+				elem.nativeElement.style.opacity = 0;
+			}
+		} else if (clone && clone.visible) {
+			clone.style.display = "none";
+			clone.visible = false;
+			elem.nativeElement.style.opacity = 1;
+		}
 	}
 
 	@ViewChild("title") enTitle!: ElementRef;
 	@ViewChild("subtitle") enSubTitle!: ElementRef;
 	@ViewChild("block2") enBlock2!: ElementRef;
-	@ViewChild("logo") enLogo!: ElementRef;
+	@ViewChild("logo") logo!: ElementRef;
 	@ViewChild("firstquote") enFirstQuote!: ElementRef;
 	@ViewChild("toolbar") enToolbar!: ElementRef;
 	@ViewChild("links") enLinks!: ElementRef;
@@ -74,10 +184,11 @@ export class AppComponent implements AfterViewInit {
 
 	@HostListener("window:scroll", ["$event"])
 	onScroll(event: Event) {
-		this.yOffset = window.pageYOffset;
+		this.yPrev = this.yCurr;
+		this.yCurr = window.pageYOffset;
 
 		let subTitleElem = this.enSubTitle.nativeElement;
-		let logoElem = this.enLogo.nativeElement;
+		let logoElem = this.logo.nativeElement;
 		let block2Elem = this.enBlock2.nativeElement;
 		let toolbarElem = this.enToolbar.nativeElement;
 		let linksElem = this.enLinks.nativeElement;
@@ -91,59 +202,46 @@ export class AppComponent implements AfterViewInit {
 		];
 
 		/**
-		 * title & subtitle fade in/out
+		 * title fade in/out
 		 */
-		this.enTitle.nativeElement.style.opacity = this.fadeOutAt(100, 100);
-
-		this.subTitle.fadeInY = !this.subTitle.fadeInY
-			? (window.innerHeight - subTitleElem.offsetTop - 60) * -1
-			: this.subTitle.fadeInY;
-		subTitleElem.style.opacity = this.fadeInAt(this.subTitle.fadeInY);
-		block2Elem.style.opacity = this.fadeInAt(
-			this.subTitle.fadeInY + subTitleElem.offsetHeight - 57.5 * 2 // 57.5 is line height
-		);
-
-		/**
-		 * subtitle & logo collision
-		 */
-		if (!this.logo.bottom) {
-			this.logo.bottom =
-				logoElem.offsetTop + logoElem.offsetHeight + TOOLBAR_HEIGHT;
-		}
+		this.fadeOut(this.enTitle, 100, 100);
+		this.fadeIn(this.enSubTitle, 0);
+		//subTitleElem.style.opacity = this.fadeInAt(0);
 
 		/**
 		 * move logo conditionally
 		 */
-		const subTitleTop = subTitleElem.offsetTop - LOGO_MARGIN - this.yOffset;
-		if (subTitleTop <= this.logo.bottom) {
-			if (
-				subTitleElem.offsetTop -
-					TOOLBAR_HEIGHT -
-					logoElem.offsetHeight -
-					this.yOffset >
-				30
-			) {
-				const logoCurrMarTop = parseInt(logoElem.style.marginTop);
-				logoElem.style.marginTop = `${
-					LOGO_MARGIN - (this.logo.bottom - subTitleTop)
-				}px`;
+		if (
+			subTitleElem.offsetTop - LOGO_MARGIN - this.yCurr <=
+			this.logoInfo.bottom
+		) {
+			const vd = subTitleElem.offsetTop - this.logoInfo.bottom;
+			if (this.logoInfo.top + vd > this.yCurr + LOGO_MARGIN + PADDING) {
+				/**
+				 * move logo until logo is at top of screen
+				 */
+				logoElem.style.marginTop = `${vd - LOGO_MARGIN - this.yCurr}px`;
 			} else {
-				if (this.logo.fadeOutY == Number.MAX_SAFE_INTEGER) {
-					this.logo.fadeOutY = this.yOffset + FADE_DURATION;
+				/**
+				 * update when to fade out the logo
+				 */
+				logoElem.style.marginTop = `${-this.logoInfo.top + PADDING}px`;
+				if (this.logoInfo.fadeOutY == Number.MAX_SAFE_INTEGER) {
+					this.logoInfo.fadeOutY = this.yCurr + FADE_DURATION;
 				}
 			}
 		} else {
-			logoElem.style.marginTop = `${LOGO_MARGIN}px`;
+			logoElem.style.marginTop = `0px`;
 		}
 
 		/**
-		 * fade out logo
+		 * fade out logo & fade in toolbar
 		 */
-		logoElem.style.opacity = this.fadeOutAt(this.logo.fadeOutY);
-		linksElem.style.opacity = this.fadeInAt(this.logo.fadeOutY);
-		lineElem.style.opacity = this.fadeInAt(this.logo.fadeOutY);
+		this.fadeOut(this.logo, this.logoInfo.fadeOutY);
+		this.fadeIn(this.enLinks, this.logoInfo.fadeOutY);
+		this.fadeIn(this.enLine, this.logoInfo.fadeOutY);
 
-		if (this.yOffset >= this.logo.fadeOutY) {
+		if (this.yCurr >= this.logoInfo.top + PADDING + FADE_DURATION) {
 			if (
 				toolbarElem.style.backgroundColor === "transparent" ||
 				toolbarElem.style.backgroundColor === ""
@@ -156,12 +254,12 @@ export class AppComponent implements AfterViewInit {
 			}
 		}
 
-		/**
-		 * fade out subtitle 1
-		 */
-		subTitleElem.style.opacity = this.fadeOutAt(
-			subTitleElem.offsetTop - TOOLBAR_HEIGHT - FADE_DURATION
-		);
+		const ft = subTitleElem.offsetTop - window.innerHeight / 2;
+		this.freeze(this.enSubTitle, ft);
+
+		this.fadeOut(this.enSubTitle, ft + FADE_DURATION);
+		this.fadeIn(this.enBlock2, ft + FADE_DURATION);
+		//}
 
 		/**
 		 * tweet animations
@@ -170,23 +268,44 @@ export class AppComponent implements AfterViewInit {
 			const t = tweet.offsetTop - window.innerHeight / 2;
 			const f = 2;
 			const d = TWEET_ROTATIONS[i];
-			const s = Math.abs(this.fadeOutAt(t) * f) + 1;
+			const s = Math.abs(this.negLinearSeq(t) * f) + 1;
 			tweet.style.transform = `scale(${s > 2 ? 2 : s}) rotate(${
-				d + this.fadeOutAt(t) * 20
+				d + this.negLinearSeq(t) * 20
 			}deg)`;
-			tweet.style.opacity = this.fadeInAt(t - FADE_DURATION / 1.5);
+			tweet.style.opacity = this.posLinearSeq(t - FADE_DURATION / 1.5);
 		});
 	}
 
-	ngAfterViewInit() {
-		let titleElem = this.enTitle.nativeElement;
-		let h = window.innerHeight - parseInt(titleElem.offsetHeight) - 100;
-		h = h > 750 ? 750 : h;
-		this.enLogo.nativeElement.style.height = `${h}px`;
-		this.enLogo.nativeElement.style.marginTop = `${LOGO_MARGIN}px`;
+	initAnimInfo() {
+		/**
+		 * calculate bottom of logo
+		 */
+		this.logoInfo.bottom =
+			this.logo.nativeElement.offsetTop +
+			this.logo.nativeElement.offsetHeight;
+		this.logoInfo.top = this.logo.nativeElement.offsetTop;
+
+		/**
+		 * update subtitle margin top based on logo height
+		 */
 		this.enSubTitle.nativeElement.style.marginTop = `${
-			window.innerHeight - 200
+			this.logoInfo.bottom +
+			LOGO_MARGIN -
+			this.enTitle.nativeElement.offsetHeight -
+			this.enTitle.nativeElement.offsetTop -
+			100
 		}px`;
+	}
+
+	ngAfterViewInit() {
 		(<any>window).twttr.widgets.load();
+
+		window.onpageshow = () => {
+			this.initAnimInfo();
+		};
+
+		window.onresize = () => {
+			this.initAnimInfo();
+		};
 	}
 }
